1ea707dcf9dfbb41b6b74d167b0aa105a1a1cf1e
lorincz_attila <l.atee.1995@gmail.com>
2018-12-05 20:21:51 +0100

    client is able to send own position, server can send heightmap data

--- PtgWeb/PtgWeb/babylonClient/src/index.ts
+++ PtgWeb/PtgWeb/babylonClient/src/index.ts
@@ -1,5 +1,78 @@
-import { Scene, Engine, Vector3 } from 'babylonjs';
+import { Scene, Engine, Vector3, UniversalCamera, Color3, Mesh } from 'babylonjs';
 import * as signalR from "@aspnet/signalr";
+
+document.addEventListener('DOMContentLoaded', startGame);
+
+const resolution = { width: 1920, height: 1080 };
+let scene: Scene;
+let engine: Engine;
+let canvas: HTMLCanvasElement;
+let camera: UniversalCamera;
+
+window.addEventListener('keypress', function (e) {
+    this.console.log('keypress event raised');
+    if (e.keyCode === 32) {
+        signalRTester();
+    }
+}, false);
+
+function signalRTester() {
+    sendPosition(camera.position);
+}
+
+function startGame() {
+    canvas = <HTMLCanvasElement> document.getElementById('renderCanvas');
+
+    canvas.style.width = `${resolution.width}px`;
+    canvas.style.height = `${resolution.height}px`;
+    engine = new Engine(canvas, true);
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+
+    scene = createScene();
+
+    engine.runRenderLoop(renderLoop);
+}
+
+function createScene(): Scene {
+    scene = new Scene(engine);
+    scene.gravity = new Vector3(0, -1, 0);
+    scene.collisionsEnabled = true;
+
+    var ground = Mesh.CreateGround('myGround', 100, 100, 10, scene);
+    ground.checkCollisions = true;
+    camera = createFreeCamera(scene);
+    var light = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(0, -1, 0), scene);
+    light.intensity = 0.5;
+    return scene;
+}
+
+function createFreeCamera(scene) {
+    let cam = new UniversalCamera('universalCamera', new BABYLON.Vector3(0, 5, -10), scene);
+    cam.attachControl(canvas);
+    cam.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+    cam.checkCollisions = true;
+    cam.applyGravity = true;
+
+    cam.keysUp.push('w'.charCodeAt(0));
+    cam.keysUp.push('W'.charCodeAt(0));
+
+    cam.keysDown.push('s'.charCodeAt(0));
+    cam.keysDown.push('S'.charCodeAt(0));
+    
+    cam.keysLeft.push('a'.charCodeAt(0));
+    cam.keysLeft.push('A'.charCodeAt(0));
+
+    cam.keysRight.push('d'.charCodeAt(0));
+    cam.keysRight.push('D'.charCodeAt(0));
+
+    return cam;
+}
+
+function renderLoop() {
+    scene.render();
+}
+
 
 const connection = new signalR.HubConnectionBuilder()
     .withUrl("/heightmap")
